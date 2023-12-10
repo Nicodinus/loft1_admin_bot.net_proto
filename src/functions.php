94a1b38b98162ed8d61a2b45b345cd8382a68085
Aleksei Iarovoi (Nicodinus) <nicodinus@yandex.ru>
2023-12-11 02:21:58 +0300

    update

--- src/functions.php
+++ src/functions.php
@@ -2,6 +2,11 @@
 
 namespace Nicodinus\Loft1AdminBot\NetProto;
 
+/**
+ * @param \Throwable $exception
+ *
+ * @return array{class: string, message: string, code: int, file: string, line: int}
+ */
 function serializeException(\Throwable $exception): array
 {
     return [
@@ -15,6 +20,13 @@ function serializeException(\Throwable $exception): array
     ];
 }
 
+/**
+ * @param array{class: string, message: string, code: int, file: string, line: int} $arr
+ *
+ * @return \Throwable
+ *
+ * @throws \ReflectionException
+ */
 function unserializeException(array $arr): \Throwable
 {
     $props = [
@@ -28,6 +40,9 @@ function unserializeException(array $arr): \Throwable
     $instance = $reflector->newInstanceWithoutConstructor();
 
     foreach ($props as $prop) {
+        if (!isset($arr[$prop])) {
+            continue;
+        }
         $_prop = $reflector->getProperty($prop);
         $_prop->setAccessible(true);
         $_prop->setValue($instance, $arr[$prop]);
